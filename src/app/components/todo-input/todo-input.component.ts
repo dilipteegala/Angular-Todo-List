1af8cb2540ed5f184328453a0fc92fe797b00ef2
dilipteegala <diliprao0007@gmail.com>
2019-01-29 11:54:06 +0530

    todoliststarter

---
+++ src/app/components/todo-input/todo-input.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+import { Todo } from '../../classes/todo';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo-input',
+  templateUrl: './todo-input.component.html',
+  styleUrls: ['./todo-input.component.css']
+})
+export class TodoInputComponent implements OnInit {
+  onEnter(Todo: string) { this.todoText = Todo;}
+
+  public todoText: string;
+
+  constructor(private todoService: TodoService) {
+    this.todoText = '';
+  }
+ 
+
+  ngOnInit() {
+  }
+
+  private addTodo(): void {
+    this.todoService.addTodo(this.todoText);
+    this.todoText = '';
+    
+  }
+}
---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Todo } from '../classes/todo';
+
+@Injectable()
+export class TodoService {
+
+  private todos: Todo[];
+  private nextId: number;
+
+  constructor() {
+    this.todos = [
+      new Todo(0, ""),
+     
+    ];
+
+    this.nextId = 0;
+  }
+
+  public addTodo(text: string): void {
+    let todo = new Todo(this.nextId, text);
+    this.todos.push(todo);
+    this.nextId++;
+  }
+
+ 
+
+  public getTodos(): Todo[] {
+    return this.todos;
+  }
+
+  public removeTodo(id: number): void {
+    this.todos = this.todos.filter((todo)=> todo.id != id);
+  }
+
+}
